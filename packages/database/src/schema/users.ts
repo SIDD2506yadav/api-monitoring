ff8b0b86717266c9a2d6a402bb0dd779109f0a87
Siddhartha <60682055+SIDD2506yadav@users.noreply.github.com>
2026-08-20 12:44:15 +0530

    feat(database): add users schema

---
+++ packages/database/src/schema/users.ts
@@ -0,0 +1,17 @@
+import {
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
